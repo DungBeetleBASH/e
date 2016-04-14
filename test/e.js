35d9b439f766cb59f5964ca47c459bf44afcf54a
Robert Hill <dr.pappa@gmail.com>
2016-04-14 20:03:11 +0100

    Unit test for: on, once, fire

--- test/e.js
+++ test/e.js
@@ -1,9 +1,86 @@
-var fn = require('../e');
+var e = require('../e'),
+	sinon = require('sinon');
 
-describe("e tests", function() {
+describe("@dbb/e tests", function() {
+
+	var func1,
+		func2,
+		eventMap;
+
+	beforeEach(function() {
+		func1 = sinon.spy();
+		func2 = sinon.spy();
+		e.removeAll();
+		eventMap = {};
+	});
+
+	afterEach(function() {
+	});
 
 	describe("on()", function() {
-		it("Bind a callback to an event and an optional context", function() {
+		// TODO: context and once
+		it("Should populate the event map", function() {
+			e.on("someEvent", func1);
+			eventMap = e.getEvents();
+			expect(eventMap["someEvent"].length).toBe(1);
+			expect(eventMap["someEvent"][0].callback).toBe(func1);
+		});
+	});
+
+	describe("once()", function() {
+		// TODO: context
+		it("Should remove the callback from the event map after it is fired and remove the event", function() {
+			e.once("someEvent", func1);
+			e.on("someOtherEvent", func2);
+			e.fire("someEvent");
+			eventMap = e.getEvents();
+			expect(func1.calledOnce).toBe(true);
+			expect(func2.called).toBe(false);
+			expect(eventMap["someEvent"]).toBe(undefined);
+			expect(eventMap["someOtherEvent"].length).toBe(1);
+			expect(eventMap["someOtherEvent"][0].callback).toBe(func2);
+		});
+		it("Should remove the callback from the event map after it is fired and keep the event", function() {
+			e.once("someEvent", func1);
+			e.on("someEvent", func2);
+			e.fire("someEvent");
+			e.fire("someEvent");
+			eventMap = e.getEvents();
+			expect(func1.calledOnce).toBe(true);
+			expect(func2.calledTwice).toBe(true);
+			expect(eventMap["someEvent"].length).toBe(1);
+			expect(eventMap["someEvent"][0].callback).toBe(func2);
+		});
+	});
+
+	describe("fire()", function() {
+		it("Should call the registered functions", function() {
+			e.on("someEvent", func1);
+			e.on("someEvent", func2);
+			e.fire("someEvent");
+			eventMap = e.getEvents();
+			expect(func1.calledOnce).toBe(true);
+			expect(func2.calledOnce).toBe(true);
+		});
+	});
+
+	describe("remove()", function() {
+		it("", function() {
+		});
+	});
+
+	describe("removeAllForEvent()", function() {
+		it("", function() {
+		});
+	});
+
+	describe("removeAll()", function() {
+		it("", function() {
+		});
+	});
+
+	describe("getEvents()", function() {
+		it("", function() {
 		});
 	});
 
