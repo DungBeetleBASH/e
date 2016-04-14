a06b30d872f4af55d930c1c7e5396846fc5897f7
Robert Hill <dr.pappa@gmail.com>
2016-04-14 01:24:17 +0100

    Added empty test

---
+++ test/e.js
@@ -0,0 +1,10 @@
+var fn = require('../e');
+
+describe("e tests", function() {
+
+	describe("on()", function() {
+		it("Bind a callback to an event and an optional context", function() {
+		});
+	});
+
+});
